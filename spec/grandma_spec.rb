e189138c223dc9d115d481f192e2f1c1a14fccda
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-05-26 19:40:54 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -2,9 +2,9 @@ def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
 
   if phrase == "I LOVE YOU GRANDMA!"
     print "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase 
-    print "NO, NOT SINCE 1938!" 
-  else  
-    print "HUH?! SPEAK UP, SONNY!" 
+  elsif phrase == phrase.upcase
+    print "NO, NOT SINCE 1938!"
+  else
+    print "HUH?! SPEAK UP, SONNY!"
   end
 end
